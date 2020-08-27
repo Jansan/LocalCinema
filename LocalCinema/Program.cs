f762e58780613e28f1dba087087849b33b7e4427
Jansan <jansan-26@hotmail.com>
2020-08-27 16:40:16 +0200

    menu option 1 created

--- LocalCinema/Program.cs
+++ LocalCinema/Program.cs
@@ -29,30 +29,55 @@ namespace LocalCinema
                         case 1:
                             Console.WriteLine("How many of you are going to the cinema?");
                             int goingCinema = Int32.Parse(Console.ReadLine());
-                            string[] people = new string[goingCinema];
-
-                            for (int i = 0; i < people.Length; i++)
-                            {
-                                Console.WriteLine($"Enter people age: {i + 1}");
-                                people[i] = Console.ReadLine();
-
-                            }
+                         //   string[] personAge = new string[goingCinema];
                             
-                            int age = Int32.Parse(Console.ReadLine());
-                            if (age <= 20)
+
+
+                            double totalCost = 0;
+
+                            for (int i = 0; i < goingCinema; i++)
                             {
-                                Console.WriteLine("Youth price is 80kr");
-                            }else if(age >= 64)
-                            {
-                                Console.WriteLine("Retirement price is 90kr");
+                                Console.WriteLine($"Enter person age: {i + 1}");
+                                // personAge[i] = Console.ReadLine();
+                                var age = int.Parse(Console.ReadLine());
+
+
+
+                                if (age <= 20)
+                                {
+                                    Console.WriteLine($"Youth price 80kr");
+                                    totalCost += 80;
+                                }else if(age >= 64)
+                                {
+                                    Console.WriteLine($"Pensioner 90kr");
+                                    totalCost += 90;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Standard price 120kr");
+                                    totalCost += 120;
+                                }
+                                
                             }
-                            else
-                            {
-                                Console.WriteLine("Standard price 120kr");
-                            }
-                            Console.WriteLine("Number of people");
+
+                            Console.WriteLine($"Number of people go to cinema: {goingCinema}");
+                            Console.WriteLine($"Total cost go to cinema : {totalCost}");
+
+
+
+
+
+
+
                             break;
                         case 2:
+                            
+                            for (int i = 1; i < 11; i++)
+                            {
+                                Console.WriteLine("");
+                                string userInput = Console.ReadLine();
+                                Console.WriteLine(userInput);
+                            }
                             break;
                         case 3:
                             break;
