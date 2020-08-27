131ebff682cf84a9db040385051a71970029a48b
Jansan <jansan-26@hotmail.com>
2020-08-27 10:40:00 +0200

    program menu created

--- LocalCinema/Program.cs
+++ LocalCinema/Program.cs
@@ -6,7 +6,43 @@ namespace LocalCinema
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Declares variables that control the main loop
+            bool isRunning = true;
+
+
+            while (isRunning)
+            {
+                Console.WriteLine("Welcome to main menu entering numbers to different menu");
+                Console.WriteLine("Menu option [0] Shuts down the program");
+                Console.WriteLine("Menu option [1] Youth or pensioner");
+                Console.WriteLine("Menu option [2] Repeat ten times");
+                Console.WriteLine("Menu option [3] The third word");
+
+                if (Int32.TryParse(Console.ReadLine(), out int results))
+                {
+                    switch (results)
+                    {
+                        case 0:
+                            Console.WriteLine("The program will end.");
+                            isRunning = false;
+                            break;
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("It is incorrect menu selection.");
+                }
+                
+
+            }
         }
     }
 }
