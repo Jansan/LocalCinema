01a7d49081a129eb883d7c8557647ab70e19df9f
Jansan <jansan-26@hotmail.com>
2020-08-27 09:28:04 +0200

    Add project files.

---
+++ LocalCinema/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LocalCinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
